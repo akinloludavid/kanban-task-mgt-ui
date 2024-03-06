134eedbc7494d78e83e4e6d6535b13b02280ef93
Akinlolu Ogunleye <akinloludavid2727@gmail.com>
2024-03-06 05:42:18 +0100

    removed redundant home page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,22 +2,14 @@ import { lazy } from "react";
 import { Navigate } from 'react-router-dom'
 import WithSuspense from '../components/WithSuspense'
 import { IAppRoute } from '../types'
-import {
-    BOARD_PAGE,
-    DASHBOARD,
-    HOME,
-    LOGIN_PAGE,
-    SIGNUP_PAGE,
-} from './pathnames'
+import { BOARD_PAGE, DASHBOARD, LOGIN_PAGE, SIGNUP_PAGE } from './pathnames'
 
-const HomePage = WithSuspense(lazy(() => import('../pages/Home')))
 const SignUpPage = WithSuspense(lazy(() => import('../pages/Signup')))
 const LoginPage = WithSuspense(lazy(() => import('../pages/Login')))
 const DashboardPage = WithSuspense(lazy(() => import('../pages/Dashboard')))
 const BoardPage = WithSuspense(lazy(() => import('../pages/Board')))
 
 export const PublicRoutes: IAppRoute[] = [
-    { path: HOME, element: <HomePage /> },
     { path: SIGNUP_PAGE, element: <SignUpPage /> },
     { path: LOGIN_PAGE, element: <LoginPage /> },
     { path: '*', element: <Navigate to={LOGIN_PAGE} /> },
